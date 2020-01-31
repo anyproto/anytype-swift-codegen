7c606787e6c439d13683ec40cf5e4befd01fcc70
Dmitry Lobanov <gaussblurinc@gmail.com>
2020-01-31 12:41:34 +0300

    core: products executable has been added.

--- Package.swift
+++ Package.swift
@@ -7,9 +7,8 @@ let package = Package(
     name: "AnytypeSwiftCodegen",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "AnytypeSwiftCodegen",
-            targets: ["AnytypeSwiftCodegen"]),
+        .executable(name: "anytype-swift-codegen", targets: ["anytype-swift-codegen"]),
+        .library(name: "AnytypeSwiftCodegen", targets: ["AnytypeSwiftCodegen"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
